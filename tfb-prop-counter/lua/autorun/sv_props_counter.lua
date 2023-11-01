7ffbcd138e8b98a84099864a842caa478dcda3e0
TheFoxBoy <101681960+The-Fox-Boy@users.noreply.github.com>
2023-11-01 00:33:08 +0000

    Update sv_props_counter.lua

--- tfb-prop-counter/lua/autorun/sv_props_counter.lua
+++ tfb-prop-counter/lua/autorun/sv_props_counter.lua
@@ -4,6 +4,13 @@ hook.Add("PlayerSpawnedProp", "CountProps", function(ply, model, ent)
     propCount = propCount + 1
 end)
 
+hook.Add("EntityRemoved", "RemoveProps", function(ent)
+    if ent:IsValid() and ent:GetClass() == "prop_physics" then
+        propCount = math.max(0, propCount - 1)
+    end
+end)
+
+
 hook.Add("PlayerSay", "CheckPropsCommand", function(ply, text, isTeam)
     if text == "!props" then
         if IsValid(ply) then
@@ -11,4 +18,3 @@ hook.Add("PlayerSay", "CheckPropsCommand", function(ply, text, isTeam)
         end
     end
 end)
-
